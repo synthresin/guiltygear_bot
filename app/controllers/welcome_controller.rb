1787ebaa61957dddf8640e09e8cf22836c807b65
synthresin <synthresin@me.com>
2016-09-10 05:47:15 +0900

    김길티 첫페이지에서 플매방 목록 보여줌

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-
+    redis = Redis.new(host: 'localhost', port: 6379)
+    @rooms = redis.lrange('guiltygear:rooms', 0, -1)
   end
 end
