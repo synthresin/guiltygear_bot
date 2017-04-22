31c81cc399348177beecf00578f273d046ffa3be
synthresin <synthresin@me.com>
2017-04-22 22:47:20 +0900

    데이터베이스 사용 && 생성 시간 추가

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    redis = Redis.new(host: 'localhost', port: 6379)
-    @rooms = redis.lrange('guiltygear:rooms', 0, -1)
+    @rooms = Room.all
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,27 @@
 class Room < ApplicationRecord
+  room_regex = /\A[a-z0-9]{4}\z/i
+
+  validates :code, uniqueness: { message: '%{value}는 이미 존재하는 플매방 번호임.'},
+                   presence: { message: '추가할 플매방 번호를 빼먹음. 예) 김길티 추가 39fh'},
+                   format: {
+                     with: room_regex,
+                     message: "%{value}는 잘못된 방번호임. 4자리 숫자-알파벳 조합만 가능.",
+                     allow_blank: true
+                   }
+
+  def bot_error_message
+    errors.map { |attr, msg| msg }.join(" ")
+  end
+
+  def self.list_message
+    return "현재 플매방 없음" if Room.count == 0
+
+    room_infos = Room.all.map do |room|
+      "*#{room.code}*        #{room.created_at.strftime('%m/%d %H:%M')} 생성"
+    end
+
+    room_infos.inject("*현재 플매방 목록*") do |full_message, room_info|
+      full_message + "\n#{room_info}"
+    end
+  end
 end
